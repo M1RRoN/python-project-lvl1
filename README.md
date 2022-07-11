d47bc613e2ac6d69fc1ceb6f772ed4129e2d92ff
M1RRoN <mds.myst@gmail.com>
2022-07-11 19:45:54 +0400

    add step 6

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/c5e0282f93ce616cb9fb/maintainability)](https://codeclimate.com/github/M1RRoN/python-project-lvl1/maintainability)
 
-https://asciinema.org/a/6GDHkA1yRyBZxlfVX039zbTtd
+asciinema step 5 - https://asciinema.org/a/6GDHkA1yRyBZxlfVX039zbTtd
+asciinema step 6 - https://asciinema.org/a/ndWaB98cEx0qPe1Pdqs5TlLof
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+import random
+import prompt
+from brain_games.scripts.brain_games import greet
+
+
+def main():
+    greet()
+    name = prompt.string("May I have your name?")
+    print(f"Hello, {name}!")
+    print('What is the result of the expression?')
+    i = 0
+    while i < 3:
+        num1 = random.randint(0, 20)
+        num2 = random.randint(0, 20)
+        expression = f"{num1} {random.choice('+-*')} {num2}"
+        result = eval(expression)
+        print(f'Question: {expression}')
+        answer = prompt.string("You answer: ")
+        if int(answer) == int(result):
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
+            print(f"Let's try again, {name}!")
+            break
+        i += 1
+        if i == 3:
+            print(f"Congratulations, {name}!")
+
+        
+
