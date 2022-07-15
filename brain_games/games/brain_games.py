416c17b7e24497561b1e131f25e2db9177dedc90
M1RRoN <mds.myst@gmail.com>
2022-07-15 17:42:45 +0400

    fix pyproject.toml

--- brain_games/games/brain_games.py
+++ brain_games/games/brain_games.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-from brain_games.cli import welcome_user
 
 
-def greet():
-    print("Welcome to the Brain Games!")
+from brain_games.scripts.brain_logic import brain_calc, brain_even, brain_gcd, brain_prime, brain_progression, greet
 
 
 def main():
-    greet()
-    welcome_user()
-
+    greet
+    brain_calc
+    brain_even
+    brain_gcd
+    brain_prime
+    brain_progression
+    
 
 if __name__ == '__main__':
     main()
