ea1f52abb9e3789d99dfa72d6b5c4b03ffab2e67
M1RRoN <mds.myst@gmail.com>
2022-07-27 17:06:08 +0400

    last fix

--- README.md
+++ README.md
@@ -8,3 +8,26 @@ asciinema step 6 - https://asciinema.org/a/ndWaB98cEx0qPe1Pdqs5TlLof
 asciinema step 7 - https://asciinema.org/a/8au4qw2cFeov6njVFT0sWKla8
 asciinema step 8 - https://asciinema.org/a/N6zrcqCztcyESpXKnJdkHdhcY
 asciinema step 9 - https://asciinema.org/a/UfZiJtqz4TEqZatNcdWD91FQJ
+
+Installation:
+Make sure you are running at least Python 3.8.10
+$ git clone https://github.com/M1RRoN/python-project-lvl1
+install:
+		poetry install
+
+build:
+		poetry build
+
+package-install:
+		python3 -m pip install --user --force dist/*.whl
+
+brain-games:
+		poetry run brain-games
+
+Start Game:
+$ brain-games
+$ brain-calc
+$ brain-even
+$ brain-gcd
+$ brain-prime
+$ brain-progression
--- brain_games/scripts/brain_logic.py
+++ brain_games/scripts/brain_logic.py
@@ -3,138 +3,113 @@
 
 import prompt
 import random
+import math
 
 
-message = "is wrong answer ;(. Correct answer was"
+message = 'is wrong answer ;(. Correct answer was'
 
 
 def greet():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
 
 
-def welcome_user():
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+def player_name():
+    global name
+    name = prompt.string('May I have your name?')
+    print(f'Hello, {name}!')
+
+
+def comparison(answer, correct_answer):
+    if answer == correct_answer:
+        print('Correct!')
+    else:
+        print(f"{answer} {message} '{correct_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
 
 
 def brain_calc():
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    player_name()
     print('What is the result of the expression?')
     i = 0
     while i < 3:
         num1 = random.randint(0, 20)
         num2 = random.randint(0, 20)
         expression = f"{num1} {random.choice('+-*')} {num2}"
-        result = eval(expression)
+        correct_answer = eval(expression)
         print(f'Question: {expression}')
-        answer = prompt.string("You answer: ")
-        if int(answer) == int(result):
-            print('Correct!')
-        else:
-            print(f"'{answer}' {message} '{result}'.")
-            print(f"Let's try again, {name}!")
+        answer = prompt.string('You answer: ')
+        if comparison(int(answer), int(correct_answer)) is False:
             break
         i += 1
         if i == 3:
-            print(f"Congratulations, {name}!")
+            print(f'Congratulations, {name}!')
 
 
 def brain_even():
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    player_name()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 0
     while i < 3:
         number = random.randint(0, 100)
         print(f'Question: {number}')
-        answer = prompt.string("You answer: ")
+        answer = prompt.string('You answer: ')
         if number % 2 == 0:
-            if answer == "yes":
-                print('Correct!')
-            else:
-                print(f"{answer} {message} 'yes'.")
-                print(f"Let's try again, {name}!")
+            correct_answer = 'yes'
+            if comparison(answer, correct_answer) is False:
                 break
         else:
-            if answer == "yes":
-                print(f"{answer} {message} 'no'.")
-                print(f"Let's try again, {name}!")
+            correct_answer = 'no'
+            if comparison(answer, correct_answer) is False:
                 break
-            else:
-                print('Correct!')
         i += 1
         if i == 3:
-            print(f"Congratulations, {name}!")
+            print(f'Congratulations, {name}!')
 
 
 def brain_gcd():
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    player_name()
     print('Find the greatest common divisor of given numbers.')
     i = 0
     while i < 3:
         num1 = random.randint(1, 100)
         num2 = random.randint(1, 100)
         print(f'Question: {num1} {num2}')
-        answer = prompt.string("You answer: ")
-        if int(answer) == gcd(num1, num2):
-            print('Correct!')
-        else:
-            print(f"'{answer}' {message} '{gcd(num1, num2)}'.")
-            print(f"Let's try again, {name}!")
+        answer = prompt.string('You answer: ')
+        if comparison(int(answer), math.gcd(num1, num2)) is False:
             break
         i += 1
         if i == 3:
             print(f"Congratulations, {name}!")
 
 
-def gcd(num1, num2):
-    while num1 != num2:
-        if num1 > num2:
-            num1 = num1 - num2
-        else:
-            num2 = num2 - num1
-    return(num1)
-
-
 def brain_prime():  # noqa: C901
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    player_name()
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     i = 0
     while i < 3:
         number = random.randint(1, 100)
         print(f'Question: {number}')
-        answer = prompt.string("You answer: ")
+        answer = prompt.string('You answer: ')
         k = 0
         for j in range(2, number // 2 + 1):
             if number % j == 0:
                 k += 1
         if k <= 0:
-            correct_answer = "yes"
-            if answer == correct_answer:
-                print('Correct!')
-            else:
-                print(f"{answer} {message} '{correct_answer}'.")
-                print(f"Let's try again, {name}!")
+            correct_answer = 'yes'
+            if comparison(answer, correct_answer) is False:
                 break
         else:
-            correct_answer = "no"
-            if answer == correct_answer:
-                print('Correct!')
-            else:
-                print(f"{answer} {message} '{correct_answer}'.")
-                print(f"Let's try again, {name}!")
+            correct_answer = 'no'
+            if comparison(answer, correct_answer) is False:
                 break
         i += 1
         if i == 3:
-            print(f"Congratulations, {name}!")
+            print(f'Congratulations, {name}!')
 
 
 def brain_progression():
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    player_name()
     print('What number is missing in the progression?')
     i = 0
     while i < 3:
@@ -150,13 +125,9 @@ def brain_progression():
         numbers[random_index] = ".."
         string = " ".join(map(str, numbers[0:5]))
         print(f'Question: {string}')
-        answer = prompt.string("You answer: ")
-        if int(answer) == int(correct_answer):
-            print('Correct!')
-        else:
-            print(f"'{answer}' {message} '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
+        answer = prompt.string('You answer: ')
+        if comparison(int(answer), int(correct_answer)) is False:
             break
         i += 1
         if i == 3:
-            print(f"Congratulations, {name}!")
+            print(f'Congratulations, {name}!')
