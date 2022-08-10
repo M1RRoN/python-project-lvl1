98efbad72ef082ce51df52d0aae7acc543c01dbe
M1RRoN <mds.myst@gmail.com>
2022-08-10 12:38:39 +0400

    last fix

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -8,7 +8,7 @@ def get_question_and_answer():
     num1 = random.randint(0, 20)
     num2 = random.randint(0, 20)
     choice_of_sign = random.choice('+-*')
-    expression = f"{num1} {choice_of_sign} {num2}"
+    expression = f'{num1} {choice_of_sign} {num2}'
     if choice_of_sign == '+':
         correct_answer = num1 + num2
     elif choice_of_sign == '-':
